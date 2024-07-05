4fd32ffb7b53b7e6d81a78834a78218ff96d89cc
shriya-pandey08 <pandeyshriya10@gmail.com>
2024-07-05 23:09:45 +0530

    new codes

---
+++ addition.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class addition {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int s = a + b;
+    System.out.println("sum of two numbers is=" + s);
+  }
+}
