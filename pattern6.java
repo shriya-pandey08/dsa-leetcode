f82be60d1bef025e2729f0b175d0f965bb762cc9
shriya-pandey08 <pandeyshriya10@gmail.com>
2024-07-08 00:41:41 +0530

    trying

---
+++ pattern6.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class pattern6 {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    try {
+      for (int i = n; i >= 1; i--) {
+        for (int j = i; j >= 1; j--) {
+          System.out.print("#");
+        }
+        System.out.println();
+      }
+
+    } finally {
+      sc.close();
+    }
+  }
+}
