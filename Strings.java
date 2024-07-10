21500ea916516dcbdbf0cdb3ad0da4482994b756
shriya-pandey08 <pandeyshriya10@gmail.com>
2024-07-11 01:23:59 +0530

    strings

---
+++ Strings.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Strings {
+  public static void main(String args[]) {
+    StringBuilder sb = new StringBuilder("Tony");
+    System.out.println(sb);
+
+    System.out.println(sb.charAt(2));
+  }
+}
