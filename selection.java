7cf3cc8879a7e310e3426558b873eba648fe0372
shriya-pandey08 <pandeyshriya10@gmail.com>
2024-07-13 15:48:57 +0530

    selesction sort in java

--- selection.java
+++ selection.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class bubble {
+class selection {
   public static void printArray(int arr[]) {
     for (int i = 0; i < arr.length; i++) {
       System.out.print(arr[i] + " ");
@@ -9,15 +9,17 @@ class bubble {
   }
 
   public static void main(String args[]) {
-    int arr[] = { 7, 8, 1, 3, 2 };
+    int arr[] = { 7, 8, 3, 1, 2 };
     for (int i = 0; i < arr.length - 1; i++) {
-      for (int j = 0; j < arr.length - i - 1; j++) {
-        if (arr[j] > arr[j + 1]) {
-          int temp = arr[j];
-          arr[j] = arr[j + 1];
-          arr[j + 1] = temp;
+      int smallest = i;
+      for (int j = i + 1; j < arr.length; j++) {
+        if (arr[smallest] > arr[j]) {
+          smallest = j;
         }
       }
+      int temp = arr[smallest];
+      arr[smallest]=arr[i];
+      arr[i]=temp;
     }
     System.out.println("Sorted elements are:");
     printArray(arr);
