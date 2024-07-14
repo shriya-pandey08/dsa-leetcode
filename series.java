7cc987501d6e277081c79999c113e02fd7ef1970
shriya-pandey08 <pandeyshriya10@gmail.com>
2024-07-14 23:47:59 +0530

    series

---
+++ series.java
@@ -0,0 +1,16 @@
+class series {
+  public static void PrintSum(int i, int n, int sum) {
+    if (i == n) {
+      sum += i;
+      System.out.println(sum);
+      return;
+    }
+    sum += i;
+    PrintSum(i + 1, n, sum);
+
+  }
+
+  public static void main(String args[]) {
+    PrintSum(1, 5, 0);
+  }
+}
